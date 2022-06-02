583e9c77566e039f9c04a6220a83a7b8872151d0
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2022-06-02 12:25:47 -0300

    feat: estrutura base das requests

--- marvel-characters/src/environments/environment.ts
+++ marvel-characters/src/environments/environment.ts
@@ -1,16 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  serverUrl: 'http://gateway.marvel.com/v1/public/',
+  timeStemp: 1654177359,
+  hash: '8ca3ed3e58e3d1d156e29c66058c5240',
+  apiKey: '8f0f42ec5cb309594f39f463d6bc447b'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
